7c297da7375f47649f3a8150a618b3acffa0fc3e
Bruno Guimaraes <devgmrs@gmail.com>
2022-08-08 19:07:27 -0300

    instalacao do tailwindCSS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
 	},
 	parserOptions: {
 		ecmaVersion: "latest",
-		sourceType: "module",
+		sourceType: "commonjs",
 	},
 	rules: {
 		"no-restricted-globals": ["error", "event", "fdescribe"],
