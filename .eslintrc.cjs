168f0a69d4c2dedc972c901baa6e1ff77a6de39b
Bruno Guimaraes <devgmrs@gmail.com>
2022-08-08 17:30:10 -0300

    :wrench: chore: corrgindo o eslit2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,26 @@
-
 module.exports = {
 	env: {
 		es6: true,
 		node: true,
 	},
-	extends: ['airbnb-base', 'prettier'],
-	plugins: ['prettier'],
+	extends: ["airbnb-base", "prettier"],
+	plugins: ["prettier"],
 	globals: {
-		Atomics: 'readonly',
-		SharedArrayBuffer: 'readonly',
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly",
 	},
 	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
 	rules: {
-		'no-restricted-globals': ['error', 'event', 'fdescribe'],
-		'no-console': 'off',
-		'prettier/prettier': 'error',
-		'class-methods-use-this': 'off',
-		'no-param-reassign': 'off',
-		camelcase: 'off',
-		'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+		"no-restricted-globals": ["error", "event", "fdescribe"],
+		"no-console": "off",
+		"prettier/prettier": "error",
+		"class-methods-use-this": "off",
+		"no-param-reassign": "off",
+		camelcase: "off",
+		"no-unused-vars": ["error", { argsIgnorePattern: "next" }],
 	},
+	ignorePatterns: ["**/*.html"],
 };
