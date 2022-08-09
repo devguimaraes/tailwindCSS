6f2c92cafbec39ce824a448088bdf15bdccf692d
Bruno Guimaraes <devgmrs@gmail.com>
2022-08-09 00:38:15 -0300

    feat: estilizacao da sessão de informacões na newslleeter

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,6 @@
 module.exports = {
+	from: "/src/input.css",
+	to: "/dist/output.css",
 	plugins: {
 		tailwindcss: {},
 		autoprefixer: {},
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,25 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		fontFamily: {
-			sans: ["ui-sans-serif", "system-ui"],
-			serif: ["ui-serif", "Georgia"],
-			mono: ["ui-monospace", "SFMono-Regular"],
-			display: ["Oswald"],
-			body: ['"Open Sans"'],
-		},
 		container: {
 			center: true,
 		},
-		extend: {},
+		extend: {
+			colors: {
+				azul: {
+					claro: "#C5DFFF",
+					escuro: "#061E3C",
+					hover: "#1057B0",
+				},
+			},
+			fontFamily: {
+				sans: ["ui-sans-serif", "system-ui"],
+				serif: ["ui-serif", "Georgia"],
+				mono: ["ui-monospace", "SFMono-Regular"],
+				display: ["Oswald"],
+				body: ['"Open Sans"'],
+			},
+		},
 	},
 	plugins: [],
 };
